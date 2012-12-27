31b014d33a12033fc51486e34835efd8fac7f316
Brett Sanders <brettrsanders@gmail.com>
2012-12-27 13:11:28 -0800

    Revert to ENV[] pattern for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => "popup"
-  provider :facebook, '500475053309032', '744788c4ddc57b8665c8c8e848fb24d2'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
