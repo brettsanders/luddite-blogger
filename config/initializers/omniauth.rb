225992b9d721dbc5ea97e2303cc26b1f80cd0868
Brett Sanders <brettrsanders@gmail.com>
2012-12-27 11:49:02 -0800

    remove env'

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => "popup"
+  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => "popup"
+  provider :facebook, '500475053309032', '744788c4ddc57b8665c8c8e848fb24d2'
 end
