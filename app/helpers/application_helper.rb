4a639b24d3acbb0901e6a38a2fb52eb3cb9aa93b
Brett Sanders <brettrsanders@gmail.com>
2012-12-29 18:17:15 -0800

    Add Facebook share helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
   include FacebookShare
+
+  def facebook_share_description_text
+    if @post
+      "#{@post.title} - #{@post.text}"
+    elsif @user
+      @user.blog_title
+    else
+      "Tagline"
+    end
+  end
 end
